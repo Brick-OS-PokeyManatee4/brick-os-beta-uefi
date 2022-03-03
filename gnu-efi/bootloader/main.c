60e9115989212c2b8da4985c07eaecd04df18880
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-03-03 09:46:16 -0600

    Update main.c

--- gnu-efi/bootloader/main.c
+++ gnu-efi/bootloader/main.c
@@ -225,7 +225,7 @@ EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     EFI_MEMORY_DESCRIPTOR* Map = NULL;
     UINTN MapSize, MapKey;
     UINTN DescriptorSize;
-    UINTN32 DescriptorVersion;
+    UINT32 DescriptorVersion;
     {
 
         SystemTable->BootServices->GetMemoryMap(&MapSize, Map, &MapKey, &DescriptorSize, &DescriptorVersion);
